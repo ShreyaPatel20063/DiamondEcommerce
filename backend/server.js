f912595bcdfbcfac9d369e1a6e94d78436e71417
ShreyaPatel20063 <sap20063@gmail.com>
2024-04-08 09:04:38 +0530

    utils added

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,16 @@
 import "dotenv/config";
 import { app } from "./app.js";
-import bodyParser from "body-parser";
 import dbconnect from "./db/dbconnect.js";
 
 const port = process.env.PORT || 3000;
 
-
-app.use(bodyParser.json());
-
 dbconnect()
-.then(()=>{
-  app.listen(port, () => {
-    console.log("server running successfully on port ", port);
-  });
-})
-.catch((err)=>{
-  console.log("server failed to start due to ", err);
-})
+  .then(()=>{
+    app.listen(port, () => {
+      console.log("server running successfully on port ", port);
+    });
+  })
+  .catch((err)=>{
+    console.log("server failed to start due to ", err);
+  })
 
